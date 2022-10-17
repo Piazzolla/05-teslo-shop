43d3e41f280c81ba7b4ecfaadc828b84c2fdfff8
Mariano <macacozzz@hotmail.com>
2022-10-17 16:28:20 -0300

    estructura de directorios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+import { ShopLayout } from '../components/layouts/ShopLayout';
 
 const Home: NextPage = () => {
   return (
-   <h1> Teslo Shop </h1>
+  <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos de Teslo Shop aqui'}>
+    <Typography variant='h1' component='h1'>Tienda</Typography>
+    <Typography variant='h2' sx={{ mb: 1 }} >Todos los productos</Typography>
+  </ShopLayout>
   )
 }
 
