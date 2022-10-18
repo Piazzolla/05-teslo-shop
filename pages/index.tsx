e653e47aa2af07bb71e7af2526dea56f417753d1
Mariano <macacozzz@hotmail.com>
2022-10-18 08:34:28 -0300

    home page temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts/ShopLayout';
+import { initialData } from '../database/products';
+
+
 
 const Home: NextPage = () => {
   return (
-  <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos de Teslo Shop aqui'}>
-    <Typography variant='h1' component='h1'>Tienda</Typography>
-    <Typography variant='h2' sx={{ mb: 1 }} >Todos los productos</Typography>
-  </ShopLayout>
+    <ShopLayout title={'Teslo-Shop'} pageDescription={'Encuentra los mejores productos de Teslo Shop aqui'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }} >Todos los productos</Typography>
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${ product.images[0]}`}
+                    alt={ product.title }
+                  >
+
+                  </CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+
+          ))
+        }
+      </Grid>
+    </ShopLayout>
   )
 }
 
