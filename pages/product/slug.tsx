ba0f612b074d056feea48e70a0b9b19d1accd15a
Mariano <macacozzz@hotmail.com>
2022-10-18 18:24:15 -0300

    item counter y selector de talle

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,8 @@
-import { Grid,Box, Typography, Button, Chip } from "@mui/material";
+import { Grid, Box, Typography, Button, Chip } from "@mui/material";
 import { ShopLayout } from "../../components/layouts";
+import { SizeSelector } from "../../components/products";
 import ProductSlideshow from "../../components/products/ProductSlideshow";
+import { ItemCounter } from "../../components/ui";
 import { initialData } from '../../database/products';
 
 
@@ -8,36 +10,39 @@ const product = initialData.products[0]; //temporal
 
 export const ProductPage = () => {
   return (
-    <ShopLayout title={product.title} pageDescription={ product.description }>
+    <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
-        <Grid item xs={ 12 } sm={ 7 }>
-          <ProductSlideshow 
-            images = { product.images }
+        <Grid item xs={12} sm={7}>
+          <ProductSlideshow
+            images={product.images}
           />
         </Grid>
-        <Grid item xs={ 12 } sm={ 5 }>
+        <Grid item xs={12} sm={5}>
           <Box display='flex' flexDirection='column'>
-              {/* titulos */}
-              <Typography variant='h1' component='h1'>{ product.title }</Typography>
-              <Typography variant='subtitle1' component='h2'>${ product.price }</Typography>
+            {/* titulos */}
+            <Typography variant='h1' component='h1'>{product.title}</Typography>
+            <Typography variant='subtitle1' component='h2'>${product.price}</Typography>
 
-              {/* Cantidad */}
-              <Box sx={{ my: 2 }}>
-                <Typography variant='subtitle2'>Cantidad</Typography>
-                {/* ItemConunter */}
-              </Box>
+            {/* Cantidad */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant='subtitle2'>Cantidad</Typography>
+              <ItemCounter />
+              <SizeSelector
+                // selectedSize={product.sizes[0]}
+                sizes={product.sizes} />
+            </Box>
 
-              {/* Agregar al carrito */}
-              <Button color="secondary" className='circular-btn'>
-                Agregar al carrito
-              </Button>
+            {/* Agregar al carrito */}
+            <Button color="secondary" className='circular-btn'>
+              Agregar al carrito
+            </Button>
 
-              {/* <Chip label='No hay disponibles' color="error" variant="outlined"/> */}
-              {/* Descripcion */}
-              <Box sx={{ mt: 3}}>
-                <Typography variant='subtitle2'>Descripción</Typography>
-                <Typography variant='body2'>{ product.description }</Typography>
-              </Box>
+            {/* <Chip label='No hay disponibles' color="error" variant="outlined"/> */}
+            {/* Descripcion */}
+            <Box sx={{ mt: 3 }}>
+              <Typography variant='subtitle2'>Descripción</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
+            </Box>
           </Box>
         </Grid>
       </Grid>
