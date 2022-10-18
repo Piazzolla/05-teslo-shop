aa8ecf1911c62278c14e8d80704eabfd10e56fa5
Mariano <macacozzz@hotmail.com>
2022-10-18 09:08:45 -0300

    lista de productos

---
+++ interfaces/products.ts
@@ -0,0 +1,16 @@
+export interface IProduct {
+    _id: string;
+    description: string;
+    images: string[];
+    inStock: number;
+    price: number;
+    sizes: ISizes[];
+    slug: string;
+    tags: string[];
+    title: string;
+    type: ValidTypes;
+    gender: 'men'|'women'|'kid'|'unisex'
+}
+
+export type ISizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
+export type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
