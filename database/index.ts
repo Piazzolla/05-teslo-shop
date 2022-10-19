512460bc4469949ff44b02bc3a9281372dd94edf
Mariano <macacozzz@hotmail.com>
2022-10-19 14:28:22 -0300

    endpoint para seed de db

--- database/index.ts
+++ database/index.ts
@@ -1,1 +1,4 @@
 export * as db from './db'; 
+
+// Base de datos de prueba
+export * as seedDatabase from './products'
--- interfaces/products.ts
+++ interfaces/products.ts
@@ -11,6 +11,8 @@ export interface IProduct {
     type: IType;
     gender: 'men'|'women'|'kid'|'unisex'
 
+    createdAt: string;
+    updatedAt: string;
 }
 
 export type ISize = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
