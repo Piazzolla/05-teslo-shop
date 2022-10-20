80126980c3222ecada7db22973fdc712e876a724
Mariano <macacozzz@hotmail.com>
2022-10-20 15:47:00 -0300

    agrego contexto para el estado del side menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,20 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { lightTheme } from '../themes';
 import { SWRConfig } from 'swr';
- 
+import { UiProvider } from '../context';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig
       value={{
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
       }}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UiProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
     </SWRConfig>
   )
 }
