f380a3fbeb038c4d2eeb16aaa94ebcd329feab02
Mariano <macacozzz@hotmail.com>
2022-10-21 15:09:30 -0300

    contexto para el carrito de compras

---
+++ context/cart/CartProvider.tsx
@@ -0,0 +1,30 @@
+import { FC, useReducer } from 'react';
+import { ICartProduct } from '../../interfaces';
+import { CartContext, cartReducer } from './';
+
+export interface CartState {
+   cart: ICartProduct[];
+}
+
+const CART_INITIAL_STATE: CartState = {
+   cart: []
+}
+
+
+type Props = {
+   children?: React.ReactNode
+  };
+
+export const CartProvider:FC<Props> = ({ children }) => {
+
+
+   const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE)
+
+   return (
+       <CartContext.Provider value={{
+           ...state
+       }}>
+           { children }
+       </CartContext.Provider>
+   )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { lightTheme } from '../themes';
 import { SWRConfig } from 'swr';
-import { UiProvider } from '../context';
+import { UiProvider, CartProvider } from '../context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,12 +11,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       value={{
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
       }}>
-      <UiProvider>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   )
 }
