31777ee25ec901063ddc895f69421bea43c2ab19
Mariano <macacozzz@hotmail.com>
2022-10-22 20:56:13 -0300

    manejo el carrito con cookies

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,6 +1,7 @@
-import { FC, useReducer } from 'react';
+import { FC, useReducer, useEffect } from 'react';
 import { ICartProduct } from '../../interfaces';
 import { CartContext, cartReducer } from './';
+import Cookie from 'js-cookie';
 
 export interface CartState {
    cart: ICartProduct[];
@@ -19,6 +20,32 @@ export const CartProvider:FC<Props> = ({ children }) => {
 
 
    const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE)
+
+
+   useEffect(() => {
+      const cookieCart = Cookie.get('cart'); 
+      if(!cookieCart) { return; }
+
+      const cookieCartParsed: ICartProduct[] = JSON.parse(cookieCart);
+
+
+      /* 
+         Las 3 lineas de codigo anteriores se pueden hacer en una asi: 
+      const cookieCart = Cookie.get('cart') ? JSON.parse( Cookie.get('cart')!): []
+      si el .get devuelve algo aviso al compilador que tengo un resultado con el ! 
+      si no devuelve nada asigno un array vacio a mi variable. 
+      */
+      if(cookieCartParsed.length === 0) { return; }
+
+      dispatch({ type: "[Cart] - LoadCart from cookies | storage", payload: cookieCartParsed })
+
+   }, [])
+
+
+   useEffect(() => {
+      Cookie.set('cart', JSON.stringify( state.cart ));
+   }, [state.cart])
+   
 
    const addProductToCart = ( product:ICartProduct) => {
      // asi no
--- context/cart/cartReducer.ts
+++ context/cart/cartReducer.ts
@@ -12,6 +12,7 @@ export const cartReducer = (state: CartState, action: CartActionType): CartState
        case '[Cart] - LoadCart from cookies | storage':
           return {
                ...state,
+               cart: [ ...action.payload ]
            }
        case '[Cart] - Update Products in Cart':
           return {
