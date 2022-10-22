f9d341b2926b8c30c0dc017046e58ef8cf2d3446
Mariano <macacozzz@hotmail.com>
2022-10-22 20:59:44 -0300

    un cambio por si el usuario manipula la cookie y esto genera un error

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -14,77 +14,84 @@ const CART_INITIAL_STATE: CartState = {
 
 type Props = {
    children?: React.ReactNode
-  };
+};
 
-export const CartProvider:FC<Props> = ({ children }) => {
+export const CartProvider: FC<Props> = ({ children }) => {
 
 
    const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE)
 
 
    useEffect(() => {
-      const cookieCart = Cookie.get('cart'); 
-      if(!cookieCart) { return; }
 
-      const cookieCartParsed: ICartProduct[] = JSON.parse(cookieCart);
+      try {
 
 
-      /* 
+         const cookieCart = Cookie.get('cart');
+         if (!cookieCart) { return; }
+
+         const cookieCartParsed: ICartProduct[] = JSON.parse(cookieCart);
+
+
+         /* 
          Las 3 lineas de codigo anteriores se pueden hacer en una asi: 
-      const cookieCart = Cookie.get('cart') ? JSON.parse( Cookie.get('cart')!): []
-      si el .get devuelve algo aviso al compilador que tengo un resultado con el ! 
-      si no devuelve nada asigno un array vacio a mi variable. 
-      */
-      if(cookieCartParsed.length === 0) { return; }
+         const cookieCart = Cookie.get('cart') ? JSON.parse( Cookie.get('cart')!): []
+         si el .get devuelve algo aviso al compilador que tengo un resultado con el ! 
+         si no devuelve nada asigno un array vacio a mi variable. 
+         */
+         if (cookieCartParsed.length === 0) { return; }
 
-      dispatch({ type: "[Cart] - LoadCart from cookies | storage", payload: cookieCartParsed })
+         dispatch({ type: "[Cart] - LoadCart from cookies | storage", payload: cookieCartParsed })
 
+      } catch (error) {
+         dispatch({ type: "[Cart] - LoadCart from cookies | storage", payload: [] })
+      }
    }, [])
 
 
    useEffect(() => {
-      Cookie.set('cart', JSON.stringify( state.cart ));
+      Cookie.set('cart', JSON.stringify(state.cart));
    }, [state.cart])
-   
 
-   const addProductToCart = ( product:ICartProduct) => {
-     // asi no
+
+   const addProductToCart = (product: ICartProduct) => {
+      // asi no
       // dispatch({ type: '[Cart] - Add Product', payload: product})
 
       // asi tampoco
       // const productsInCart = state.cart.filter( p => p._id !== product._id && p.size !== product.size )
       // ... mandar al reducer todo el state nuevo y que el reducer lo actualice
-      
-      //asi si
-      const isProductInCart = state.cart.some( p => p._id === product._id );
-      if( ! isProductInCart ) return dispatch({ type: '[Cart] - Update Products in Cart', payload: [ ...state.cart, product ]})
 
-      const isProductInCartButDifferentSize = state.cart.some( p => p._id === product._id && p.size === product.size);
-      if( !isProductInCartButDifferentSize ) return dispatch({ type: '[Cart] - Update Products in Cart', payload: [ ...state.cart, product ]})
+      //asi si
+      const isProductInCart = state.cart.some(p => p._id === product._id);
+      if (!isProductInCart) return dispatch({ type: '[Cart] - Update Products in Cart', payload: [...state.cart, product] })
+
+      const isProductInCartButDifferentSize = state.cart.some(p => p._id === product._id && p.size === product.size);
+      if (!isProductInCartButDifferentSize) return dispatch({ type: '[Cart] - Update Products in Cart', payload: [...state.cart, product] })
 
 
       // Si llegamos aca, es porque existe el producto y en la misma talla. Acumular.
-      const updatedProducts = state.cart.map( p => {
-         if (p._id !== product._id ) return p;
-         if (p.size !== product.size ) return p;
+      const updatedProducts = state.cart.map(p => {
+         if (p._id !== product._id) return p;
+         if (p.size !== product.size) return p;
 
          // Actualizar cantidad
          p.quantity += product.quantity;
          return p;
       })
 
-      dispatch({ type: '[Cart] - Update Products in Cart', payload: updatedProducts})
+      dispatch({ type: '[Cart] - Update Products in Cart', payload: updatedProducts })
 
    }
 
    return (
-       <CartContext.Provider value={{
-           ...state,
+      <CartContext.Provider value={{
+         ...state,
 
-           //methods
-           addProductToCart
-       }}>
-           { children }
-       </CartContext.Provider>
+         //methods
+         addProductToCart
+      }}>
+         {children}
+      </CartContext.Provider>
    )
 }
