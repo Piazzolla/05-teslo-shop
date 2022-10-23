df1e23d401992b6b80dfcd4e1314cfad8031a34a
Mariano <macacozzz@hotmail.com>
2022-10-23 15:36:55 -0300

    tomo los valores del cart en el checkout

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,14 +1,9 @@
 import NextLink from 'next/link';
 import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
-import { initialData } from '../../database/products';
 import { ItemCounter } from '../ui';
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { CartContext } from '../../context';
 
-const productsInCart = [
-    initialData.products[0],
-    initialData.products[1],
-    initialData.products[2],
-]
 
 interface Props {
     editable?: boolean;
@@ -16,7 +11,7 @@ interface Props {
 
 export const CartList: FC<Props> = ({ editable = false }) => {
 
-
+    const { cart: productsInCart } = useContext(CartContext)
 
     return (
         <>
@@ -29,7 +24,7 @@ export const CartList: FC<Props> = ({ editable = false }) => {
                                 <Link>
                                     <CardActionArea>
                                         <CardMedia
-                                            image={`/products/${product.images[0]}`}
+                                            image={`/products/${product.image}`}
                                             component='img'
                                             sx={{ bordarRadius: '5px' }}
                                         />
@@ -40,18 +35,23 @@ export const CartList: FC<Props> = ({ editable = false }) => {
                         <Grid item xs={7}>
                             <Box display='flex' flexDirection='column'>
                                 <Typography variant='body1'>{product.title}</Typography>
-                                <Typography variant='body1'>Talla: <strong>M</strong></Typography>
+                                <Typography variant='body1'>Talla: <strong>{product.size}</strong></Typography>
   
                                 {
                                     editable ?
+                                        (
                                         <ItemCounter 
                                             updateQuantity={ () => {} }
-                                            
-                                            /> : <Typography variant='h5'>3 items</Typography>
+                                            currentValue={ product.quantity }
+                                            maxValue={ 10 }
+                                            />
+                                            )
+                                             : 
+                                            ( <Typography variant='h5'>{ product.quantity} { product.quantity > 1? 'productos' : 'producto'}</Typography>)
                                 }
-                                <ItemCounter 
+                                {/* <ItemCounter 
                                     updateQuantity={ () => {} }
-                                />
+                                /> */}
                             </Box>
 
                         </Grid>
