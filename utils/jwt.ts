df0fc31a1ad7ca08c1e3552f0512dc25c3837d2e
Mariano <macacozzz@hotmail.com>
2022-10-25 10:46:34 -0300

    validate jwt

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,22 +1,45 @@
 import jwt from 'jsonwebtoken';
 
-/* jwt funciona en base a callbacks y no promesas asi que hacemos una conversion para que devuelva una promesa */
 
 
 export const signToken = ( _id: string, email: string) => {
     if( !process.env.JWT_SECRET_SEED ) {
         throw new Error('No hay semilla de JWT - Revisar variables de entorno');
     }
-
+    
     return jwt.sign(
         // payload, no meter info sensible aca
         { _id, email},
-
+        
         // Seed
         process.env.JWT_SECRET_SEED,
-
+        
         // Opciones
         { expiresIn: '30d' }
-    )
+        )
+        
+    }
+    
+    /* jwt funciona en base a callbacks y no promesas asi que hacemos una conversion para que devuelva una promesa */
+    
+export const isValidToken = ( token: string):Promise<string> => {
+    if( !process.env.JWT_SECRET_SEED) {
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+    }
+
+    return new Promise( (resolve, reject) => {
+         try {
+            jwt.verify( token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+                if( err ) return reject('JWT no es valido');
+
+                const { _id } = payload as { _id: string };
+
+                resolve(_id);
+
+            })
+         } catch (error) {
+            reject('JWT no es valido')            
+         }
+    })
 
 }
