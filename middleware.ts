44e6b7372dac944b5587880e74030d860400ca49
Mariano <macacozzz@hotmail.com>
2022-10-26 18:52:20 -0300

    uso una libreria externa para verificar el jwt porque con los middlewares nuevos de la version 12.2 de nextjs la util propia no sirve

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import * as jose from 'jose';
+ 
+ 
+export async function middleware(req: NextRequest) {
+ 
+    try {
+        await jose.jwtVerify(req.cookies.get('token') as string,
+            new TextEncoder().encode(process.env.JWT_SECRET_SEED));
+ 
+        return NextResponse.next();
+ 
+ 
+    } catch (error) {
+ 
+        return NextResponse.redirect(`/auth/login?p=${config.matcher}`);
+    }
+ 
+ 
+}
+
+export const config = {
+    //matcher: '/about/:path*',
+      matcher: [
+       //   '/api/:path', 
+          '/checkout/:path/'
+      ]
+  }
