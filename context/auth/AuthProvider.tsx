ed84196bff6ebc3828b0f750ecde7f04c5c14e72
Mariano <macacozzz@hotmail.com>
2022-10-27 09:37:16 -0300

    falto eliminar unas cookies

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -89,6 +89,16 @@ export const AuthProvider: FC<Props> = ({ children }) => {
    const logout = () => {
       Cookies.remove('token');
       Cookies.remove('cart');
+
+      Cookies.remove('firstName' );
+      Cookies.remove('lastName' );
+      Cookies.remove('address' );
+      Cookies.remove('address2' );
+      Cookies.remove('zip' );
+      Cookies.remove('city' );
+      Cookies.remove('country' );
+      Cookies.remove('phone' );
+
       router.reload(); //hace un refresh de la app, como ctrl r
 
    }
