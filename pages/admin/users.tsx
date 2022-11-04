5f810981d2ef1bdb1d118a13b62dc67b0237b2d5
Mariano <macacozzz@hotmail.com>
2022-11-04 12:31:30 -0300

    edicion de roles de usuario

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,24 +1,52 @@
 import { PeopleOutline } from "@mui/icons-material"
-import { AdminLayout } from "../../components/layouts"
 import { DataGrid } from '@mui/x-data-grid'
 import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid/models';
-import { Grid } from "@mui/material";
+import { Grid, MenuItem, Select } from "@mui/material";
 import useSWR from 'swr';
 import { IUser } from '../../interfaces/user';
+import { AdminLayout } from '../../components/layouts/AdminLayout';
+import { NextPage } from "next";
+import { tesloApi } from "../../api";
 
 
 
-const UsersPage = () => {
+const UsersPage: NextPage = () => {
 
     const { data, error } = useSWR<IUser[]>('/api/admin/users');
 
 
     if (!data && !error) return (<></>);
 
+
+    const onRoleUpdated = async(userId: string, newRole: string) => {
+
+        try {
+            await tesloApi.put('/admin/users', { userId, role: newRole });
+            
+        } catch (error) {
+            alert('No se pudo actualizar el rol del usuario')
+        }
+    }
+
     const columns: GridColDef[] = [
         { field: 'email', headerName: 'Correo', width: 250},
         { field: 'name', headerName: 'Nombre Completo', width: 300},
-        { field: 'role', headerName: 'Rol', width: 300},
+        { field: 'role', headerName: 'Rol', width: 300, renderCell: ({row}: GridRenderCellParams) => {
+            return ( 
+                <Select
+                    value={ row.role }
+                    label="Rol"
+                    onChange={ ({ target }) => onRoleUpdated( row.id, target.value )}
+                    sx={{ width: '300px'}}
+                >
+                    <MenuItem value='admin'>Admin</MenuItem>
+                    <MenuItem value='client'>Client</MenuItem>
+                    <MenuItem value='super-user'>Super User</MenuItem>
+                    <MenuItem value='SEO'>SEO</MenuItem>
+
+                </Select>
+            )
+        }},
     ];
 
     const rows = data!.map( user => ({
