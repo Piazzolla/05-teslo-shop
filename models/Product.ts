9c31fe79e0a0cdf4c179126469cc9522ccf38e9b
Mariano <macacozzz@hotmail.com>
2022-11-08 14:25:54 -0300

    endpoint para crear un producto nuevo

--- models/Product.ts
+++ models/Product.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, model, Model } from 'mongoose';
 import { IProduct } from '../interfaces/products';
 
 const productSchema = new Schema({
-    description: { type: String, required: true },
+    description: { type: String, required: true, default:'' },
     images: [{ type: String }],
     inStock: { type: Number, required: true, default: 0 },
     price: { type: Number, required: true, default: 0 },
@@ -13,22 +13,24 @@ const productSchema = new Schema({
             message: '{VALUE} no es un tamaño válido'
         }
     }],
-    slug: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true, default:''  },
     tags: [{ type: String }],
-    title: { type: String, required: true },
+    title: { type: String, required: true, default: '' },
     type: {
         type: String,
         enum: {
             values: ['shirts', 'pants', 'hoodies', 'hats'],
             message: '{VALUE} no es un tipo de prenda válido'
-        }
+        },
+        default: 'shirts'
     },
     gender: {
         type: String,
         enum: {
             values: ['men', 'women', 'kid', 'unisex'],
             message: '{VALUE} no es un genero valido'
-        }
+        },
+        default:'women'
     }
 },
 {
